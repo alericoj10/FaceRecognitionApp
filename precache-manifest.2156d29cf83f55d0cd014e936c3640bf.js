668ac76d3a31412d8f6812222f0ecb6d7dd0dd6d
Alejandro <arico@uc.cl>
2020-06-28 10:17:35 -0400

    Updates

---
+++ precache-manifest.2156d29cf83f55d0cd014e936c3640bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c55faa32af3b4f20e02bdd2547fc62",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "3a6ad2dd55096febbf70",
+    "url": "/face-recognition-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2009ad4b55f0c3317cd5",
+    "url": "/face-recognition-app/static/css/main.6742fa72.chunk.css"
+  },
+  {
+    "revision": "3a6ad2dd55096febbf70",
+    "url": "/face-recognition-app/static/js/2.535168aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-app/static/js/2.535168aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2009ad4b55f0c3317cd5",
+    "url": "/face-recognition-app/static/js/main.c26b01fc.chunk.js"
+  },
+  {
+    "revision": "84e4d0d0d963014966fa",
+    "url": "/face-recognition-app/static/js/runtime-main.3d1ce25a.js"
+  }
+]);
